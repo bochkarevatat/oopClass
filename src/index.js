1040c7563ce4d1945e364a7a86db236d96f57c07
bochkarevatat <tanitabt@rambler.ru>
2022-04-30 21:47:39 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './heroes/Boweman';
+import './heroes/Swordsman';
+import './heroes/Magician';
+import './heroes/Undead';
+import './heroes/Zombie';
+import './heroes/Daemon';
+import './style.css';
